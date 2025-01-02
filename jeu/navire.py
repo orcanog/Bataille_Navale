fd9d6701d5bbe0813db31e146d82463976dff019
Orcan <tricyxyt@gmail.com>
2025-01-02 16:06:32 +0100

    Ajouts des fonctions est_detruit, touche, get_position et get_taille de la classe Navire

--- jeu/navire.py
+++ jeu/navire.py
@@ -4,3 +4,15 @@ class Navire:
         self.taille = taille
         self.positions = []
         self.degats = []
+
+    def est_detruit(self):
+        return len(self.degats) == self.taille
+
+    def touche(self):
+        self.degats.append(1)
+
+    def get_position(self):
+        return self.positions
+
+    def get_taille(self):
+        return self.taille
