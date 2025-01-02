52acad014aa6e6226fbabf63374b11e6a20842e7
Orcan <tricyxyt@gmail.com>
2025-01-02 15:52:33 +0100

    Ajouts des fonctions pour placer des navires et effectuer des tirs

--- jeu/joueur.py
+++ jeu/joueur.py
@@ -3,3 +3,16 @@ class Joueur:
         self.nom = nom
         self.navires = []
         self.tirs = []
+
+    def placer_navires(self, navire, position):
+        self.navires.append((navire, position))
+
+    def effectuer_tir(self, position):
+        # Vérifier si un tir n'a pas déjà été effectué à cette position
+        if position not in self.tirs:
+            self.tirs.append(position)
+            return True
+        return False
+
+    def defaite(self):
+        pass
