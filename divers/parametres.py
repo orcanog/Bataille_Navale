8f59cefd9e232bc3c251a5dbc6a30f356f59ea15
Orcan <tricyxyt@gmail.com>
2025-01-03 11:39:31 +0100

    Ajout des paramètres d'indicateurs visuels du jeu

---
+++ divers/parametres.py
@@ -0,0 +1,10 @@
+NAVIRE_ASSETS = {
+    "Porte-avions": "#505050",
+    "Croiseur": "#404040",
+    "Destroyeur": "#303030",
+    "Sous-marin": "#101010"
+}
+
+TIR_REUSSI_ASSET = "red"
+TIR_MANQUE_ASSET = "blue"
+EAU_ASSET = "white"
